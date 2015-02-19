720017e9c8fc6260884d6bf70dd8af93916b4313
John Otander <johnotander@gmail.com>
2015-02-19 16:33:00 -0700

    1.0.0

--- test/test.js
+++ test/test.js
@@ -7,6 +7,10 @@ describe('get-query-param', function() {
     assert.equal(getQueryParam('a', 'http://foobar.com?a=hello'), 'hello')
   })
 
+  it('should return a query param even when there are multiple params', function() {
+    assert.equal(getQueryParam('foo', 'http://foobar.com?a=hello&foo=bar&baz=buzz'), 'bar')
+  })
+
   it('should not return a query param when it does not exist', function() {
     assert.equal(getQueryParam('b', 'http://foobar.com?a=hello'), undefined)
   })
